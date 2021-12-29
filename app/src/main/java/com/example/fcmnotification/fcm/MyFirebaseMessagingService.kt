c68beafe2797f11a799021d9a73fc0eb2a708715
NGUYEN HOAI NAM 03 <namnh03@ominext.com>
2021-12-29 16:00:35 +0700

    Fcm notification into activity custom

--- app/src/main/java/com/example/fcmnotification/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fcmnotification/fcm/MyFirebaseMessagingService.kt
@@ -2,14 +2,17 @@ package com.example.fcmnotification.fcm
 
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.example.fcmnotification.MainActivity
 import com.example.fcmnotification.MyApplication
 import com.example.fcmnotification.R
+import com.example.fcmnotification.ResultActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.*
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -28,17 +31,27 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     }
 
     private fun sendNotification(title: String?, message: String?) {
-        val intent = Intent(this , MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+//        val intent = Intent(this , MainActivity::class.java)
+        val resultIntent = Intent(this, ResultActivity::class.java)
+// Create the TaskStackBuilder
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+            // Add the , which intentinflates the back stack
+            addNextIntentWithParentStack(resultIntent)
+            // Get the PendingIntent containing the entire back stack
+            getPendingIntent(getNotificationId(), PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+      //  val pendingIntent = PendingIntent.getActivity(this,0,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT)
         val notificationBuilder = NotificationCompat.Builder(this,MyApplication.CHANEL_ID)
                                   .setContentTitle(title)
                                   .setContentText(message)
                                   .setSmallIcon(R.drawable.ic_notification)
-                                  .setContentIntent(pendingIntent)
+                                  .setContentIntent(resultPendingIntent)
         val notification = notificationBuilder.build()
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        with(notificationManager) { notify(1, notification) }
+        with(notificationManager) { notify(getNotificationId(), notification) }
     }
+
+    private fun getNotificationId(): Int = Date().time.toInt()
 
     companion object {
         const val TAG = "MyFirebaseMessagingService"
